82fe438220c535782e124bc19b170f28ba5a6e2b
skiwqv <andreus0212@gmail.com>
2024-06-14 22:57:30 +0300

    added gsap

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,30 @@
 <template>
   <div>ШЛЮХА!!</div>
-  <img src="../assets/images/jiafei.png" alt="" />
+  <img
+    src="../assets/images/jiafei.png"
+    alt="jiafei image"
+    class="image"
+    ref="jiafei"
+  />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import gsap from "gsap";
+const jiafei = ref("jiafei");
+const anim = () => {
+  gsap.to(jiafei.value, { repeat: -1, rotation: 360 });
+};
 
-<style></style>
+onMounted(() => {
+  anim();
+});
+</script>
+
+<style>
+.image {
+  margin-top: 200px;
+  max-width: 400px;
+  max-height: 400px;
+}
+</style>
