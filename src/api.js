3abca0895f2cb464f5eae751bd640629d983826e
skiwqv <andreus0212@gmail.com>
2024-06-14 22:23:06 +0300

    added api.js

---
+++ src/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://django-app.onrender.com/api",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
